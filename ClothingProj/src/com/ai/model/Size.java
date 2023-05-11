75f62bf45b06497500a148f9bc9ad9d448254c6d
Aung-hein-oo <132542412+Aung-hein-oo@users.noreply.github.com>
2023-05-11 14:46:15 +0630

    Update Size.java

--- ClothingProj/src/com/ai/model/Size.java
+++ ClothingProj/src/com/ai/model/Size.java
@@ -2,4 +2,18 @@ package com.ai.model;
 
 public class Size {
 
+	private int id;
+	private String value;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
